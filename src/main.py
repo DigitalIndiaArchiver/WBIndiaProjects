128bccfd59476476264e3854921609c76090ad8e
Srikanth L <srikanthlogic@users.noreply.github.com>
2023-03-15 10:04:11 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ def main():
     data = json.loads(response.text)
     projects = data['projects'].values()
     projectDataFrame = pd.DataFrame.from_dict(projects, orient='columns')
-    projectDataFrame.sort_values(by='id'ascending=False)
+    projectDataFrame.sort_values(by='id', ascending=False)
     projectDataFrame.to_csv('data/WB_India_Projects.csv')
 
 

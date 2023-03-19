3cbb023040134437c90a6ca26b65736e2ebd3779
Srikanth L <code@srik.me>
2023-03-19 06:06:51 +0000

    Add contracts

--- src/main.py
+++ src/main.py
@@ -8,8 +8,37 @@ headers = {
     'Content-Type': 'application/json'
 }
 
-PROJECTS_URL_BASE = "http://search.worldbank.org/api/v2/projects"
-DOCUMENTS_URL_BASE = "https://search.worldbank.org/api/v2/wds"
+SEARCH_API_BASE_V1 = "http://search.worldbank.org/api"
+SEARCH_API_BASE_V2 = "http://search.worldbank.org/api/v2"
+PROJECTS_URL_BASE = SEARCH_API_BASE_V2 + "/projects"
+CONTRACTS_URL_BASE = SEARCH_API_BASE_V1 + "/contractdata"
+
+DOCUMENTS_URL_BASE = SEARCH_API_BASE_V2 + "/wds"
+FINANCE_URL = 'https://finances.worldbank.org/resource/sfv5-tf7p.json'
+
+
+def paginate(url, entity, params, is_dict=True):
+    """Helper method to paginate API data"""
+    count = 0
+    i = 0
+    data = []
+    while i <= count:
+        try:
+            params['os'] = str(i)
+            response = requests.request(
+                "GET", url, headers=headers, params=params, timeout=1800)
+            response_data = json.loads(response.text)
+            if is_dict:
+                data = data + list(response_data[entity].values())
+            else:
+                data = data + response_data[entity]
+            count = int(response_data['total'])
+        except:
+            print('Exception trying to fetch ' + response.url)
+            continue
+        finally:
+            i = i + 1000
+    return data
 
 
 def get_documents():
@@ -20,22 +49,24 @@ def get_documents():
 
     response = requests.request(
         "GET", DOCUMENTS_URL_BASE, headers=headers, params=params, timeout=1800)
-    data = json.loads(response.text)
-    doc_count = data['total']
-    document_list = list(data['documents'].values())
-    i = 1000
-    while i < doc_count:
-        try:
-            params['os'] = str(i)
-            response = requests.request(
-                "GET", DOCUMENTS_URL_BASE, headers=headers, params=params, timeout=1800)
-            data = json.loads(response.text)
-            document_list = document_list + list(data['documents'].values())
-            doc_count = data['total']
-            i = i + 1000
-        except:
-            print('Exception trying to fetch ' + response.url)
-            continue
+    # data = json.loads(response.text)
+    # doc_count = data['total']
+    # document_list = list(data['documents'].values())
+    # i = 1000
+    # while i < doc_count:
+    #     try:
+    #         params['os'] = str(i)
+    #         response = requests.request(
+    #             "GET", DOCUMENTS_URL_BASE, headers=headers, params=params, timeout=1800)
+    #         data = json.loads(response.text)
+    #         document_list = document_list + list(data['documents'].values())
+    #         doc_count = data['total']
+    #         i = i + 1000
+    #     except:
+    #         print('Exception trying to fetch ' + response.url)
+    #         continue
+    document_list = paginate(url=DOCUMENTS_URL_BASE,
+                             entity='documents', params=params)
     documents_dataframe = pd.DataFrame.from_dict(
         document_list, orient='columns')
     documents_dataframe = documents_dataframe.drop(
@@ -51,15 +82,13 @@ def get_documents():
 def get_projects():
     """Get Project Information"""
     params = {'format': 'json', 'kw': 'N', 'countryname_exact': 'Republic of India', 'rows': '1000',
-              'fl': """id,regionname,countryname_mdk,prodline,borrower,lendinginstr,
+              'fl': """id,countryname_mdk,prodline,borrower,lendinginstr,
               lendinginstrcode,lendinginstrtype_mdk,supplementprojectflg,productlinetype,
               projectstatusdisplay,status,pidrecdbwind,impagency,project_name,
               boardapprovaldate,closingdate,lendprojectcost,ibrdcommamt,idacommamt,
               totalamt"""}
-    response = requests.request(
-        "GET", PROJECTS_URL_BASE, headers=headers, params=params, timeout=1800)
-    data = json.loads(response.text)
-    projects = data['projects'].values()
+    projects = paginate(url=PROJECTS_URL_BASE,
+                        entity='projects', params=params)
     project_dataframe = pd.DataFrame.from_dict(projects, orient='columns')
     project_dataframe.sort_values(by='id', ascending=False)
     project_dataframe.to_csv('data/WB_India_Projects.csv')
@@ -67,19 +96,32 @@ def get_projects():
 
 def get_finances():
     """Get Finance data"""
-    FINANCE_URL = 'https://finances.worldbank.org/resource/sfv5-tf7p.json'
-    data = json.loads(requests.request("GET",FINANCE_URL).text)
+    data = json.loads(requests.request("GET", FINANCE_URL).text)
     finance_dataframe = pd.DataFrame.from_dict(data, orient='columns')
     finance_dataframe = finance_dataframe.loc[finance_dataframe['country_code'] == 'IN']
     finance_dataframe.sort_values(by='project_id', ascending=False)
     finance_dataframe.to_csv('data/WB_India_Project_Finance.csv')
 
 
+def get_contracts():
+    """Get all contracts"""
+    params = {"countryshortname": "India", "rows": "1000",
+              "fl": """project_name,contr_id,contr_sgn_date,contr_desc,
+              total_contr_amnt,contr_refnum,procurement_group,suppinfo,
+              supplier_countryshortname,procu_meth_text,teammemfullname,
+              rvw_type"""}
+    contracts_data = paginate(url=CONTRACTS_URL_BASE,
+                              entity='contract', params=params, is_dict=False)
+    with open('data/WB_India_Contracts.json', 'w') as f:
+        json.dump(contracts_data, f)
+
+
 def main():
     """Main method"""
     get_projects()
     get_documents()
-    get_finances()
+    # get_finances()
+    get_contracts()
 
 
 if __name__ == "__main__":
